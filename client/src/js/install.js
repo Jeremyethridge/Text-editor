35498476ec1aede158c6a58b393e83aacd5fe698
JeremyEthridge <Jeremyethridge6@yahoo.com>
2023-10-27 23:28:08 -0400

    changed .gitignore, changed port, added routes for resources

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,12 +1,28 @@
 const butInstall = document.getElementById('buttonInstall');
-
+let deferredPrompt;
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    deferredPrompt = event;
+    butInstall.style.display = 'block';
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        const result = await deferredPrompt.userChoice;
+        if (result.outcome === 'accepted') {
+            console.log('App installation accepted');
+        } else {
+            console.log('App installation dismissed');
+        }
+        deferredPrompt = null;
+    }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('App installed successfully!')
+});
